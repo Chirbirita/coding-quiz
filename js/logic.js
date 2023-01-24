da350c919f874c771618a7400e580856bd4b603d
chirbirita <crisperezarranz@gmail.com>
2023-01-24 23:39:24 +0000

    create needed variables for required js functions

--- js/logic.js
+++ js/logic.js
@@ -2,7 +2,19 @@
 
 // variable to keep track of quiz progress
 let currentQuestionsIndex = 0;
-let time = questions
+let time = questions.length * 15;
+let timerID;
 
 
 //HTML elements
+let questionsElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+let choicesElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialsElement = document.getElementById("initials");
+let feedbackElement = document.getElementById("feedback");
+
+let soundRight = new Audio("/assets/sfx/correct.wav");
+
+function startQuiz
